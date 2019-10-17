b068059652d6fe194b9d83b0ed6f4c4d9fe7dddc
Oluwajoba <oluwajobabello@gmail.com>
2019-10-18 00:56:47 +0100

    add upMovieForm component / PutMovieForm / Movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -39,14 +39,15 @@ export default class Movie extends React.Component {
   };
 
   putMovie = () => {
-  this.editMovie(4, {
-      id: 5,
-      title: "Jobsy",
+    this.editMovie(4, {
+      id: 1,
+      title: "Jobsyttttt",
       director: "Bailus",
       metascore: 100,
       stars: ["Tee", "Bossy", "Sammy"]
     })
-    console.log("working");
+
+    console.log("working")
   };
 
 
@@ -61,17 +62,104 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
-        <div className="save-button2" onClick={this.putMovie}>
+        <div className="save-button2" onClick={this.putMovie} >
           Edit/Update
         </div>
+        <PutMovieForm editMovie={this.editMovie} />
       </div>
     );
   }
 }
-// editMovie(4, {
-//   id: 5,
-// title: "Jobsy",
-// director: "Bailus",
-// metascore: 100,
-// stars: ["Tee", "Bossy", "Sammy"]
-// })
+
+
+
+class PutMovieForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: {
+        id: 1,
+        title: "Jobsy77777]]]",
+        director: "Bailus",
+        metascore: 100,
+        stars: ["Tee", "Bossy", "Sammy"]
+      }
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      movies: {
+        ...this.state.movies,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  // putMessage = e => {
+  //   e.preventDefault();
+  //   // invoke this.props.putMessage here once it is written and passed in
+  //   // then pass in the movie quote that is on state as an argument
+  //   this.props.putMessage("72", this.state.movies);
+  // };
+  putMessage = e => {
+    e.preventDefault();
+    // invoke this.props.putMessage here once it is written and passed in
+    // then pass in the movie quote that is on state as an argument
+    this.props.editMovie("3", this.state.movies);
+  };
+
+  render() {
+    return (
+      <div className="quotes-form">
+        <h2>PUT (update) a quote</h2>
+        <form onSubmit={this.putMessage}>
+        <input
+            type="text"
+            name="id"
+            placeholder="ID"
+            onChange={this.handleChange}
+          value={this.state.movies.id}
+          />
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            onChange={this.handleChange}
+          value={this.state.movies.title}
+          />
+          <input
+            type="text"
+            name="director"
+            placeholder="Director"
+            onChange={this.handleChange}
+          value={this.state.movies.director}
+          />
+          <input
+            type="text"
+            name="metascore"
+            placeholder="Metascore"
+            onChange={this.handleChange}
+          value={this.state.movies.metascore}
+          />
+          <input
+            type="text"
+            name="stars"
+            placeholder="Stars"
+            onChange={this.handleChange}
+          value={this.state.movies.stars}
+          />
+          {/* {this.props.putError ? (
+            <ErrorMessage message={this.props.putError} />
+          ) : null}
+          {this.props.putSuccessMessage ? (
+            <SuccessMessage message={this.props.putSuccessMessage} />
+          ) : null} */}
+          <button className="quotes-btn" type="submit">
+            PUT quote
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
